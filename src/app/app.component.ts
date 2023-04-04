d5d66823696ccc78764876865cc24c0ace8f10c5
Ivan <zrosji@gmail.com>
2023-04-04 23:09:26 +0200

    simple camera rotation and moving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,10 @@ export class AppComponent implements AfterViewInit {
   material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   cube = new THREE.Mesh(this.geometry, this.material);
 
+  mouseDown = false;
+  mouseX = 0;
+  mouseY = 0;
+
   ngAfterViewInit(): void {
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvasRef.nativeElement,
@@ -33,9 +37,50 @@ export class AppComponent implements AfterViewInit {
     this.camera.position.z = 5;
     this.cube.rotation.x += 0.5;
 
-    setInterval(() => {
-      this.cube.rotation.y += 0.02;
-      this.renderer.render(this.scene, this.camera);
-    }, 10);
+    // setInterval(() => {
+    //   this.cube.rotation.y += 0.02;
+    //   this.renderer.render(this.scene, this.camera);
+    // }, 10);
+
+    this.canvasRef.nativeElement.addEventListener('mousedown', (event: any) => {
+      this.mouseDown = true;
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
+    });
+
+    this.canvasRef.nativeElement.addEventListener('mouseup', () => {
+      this.mouseDown = false;
+    });
+
+    this.canvasRef.nativeElement.addEventListener('mousemove', (event: any) => {
+      if (!this.mouseDown) {
+        return;
+      }
+
+      const deltaX = event.clientX - this.mouseX;
+      const deltaY = event.clientY - this.mouseY;
+
+      // Rotate camera
+      const rotationSpeed = 0.01;
+      this.camera.rotation.y -= deltaX * rotationSpeed;
+      this.camera.rotation.x -= deltaY * rotationSpeed;
+
+      this.mouseX = event.clientX;
+      this.mouseY = event.clientY;
+    });
+
+    this.canvasRef.nativeElement.addEventListener('wheel', (event: any) => {
+      // Zoom camera
+      const zoomSpeed = 0.1;
+      this.camera.position.z += event.deltaY * zoomSpeed;
+    });
+
+    this.animate();
+  }
+
+  private animate() {
+    requestAnimationFrame(() => this.animate());
+    this.cube.rotation.y += 0.02;
+    this.renderer.render(this.scene, this.camera);
   }
 }
